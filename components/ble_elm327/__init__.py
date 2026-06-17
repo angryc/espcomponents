c7eec695d893f5c3a66ce6013ca9874b098a3302
angryc <benjamin.proeger@googlemail.com>
2026-06-17 16:52:08 +0200

    Update __init__.py

--- components/ble_elm327/__init__.py
+++ components/ble_elm327/__init__.py
@@ -125,7 +125,18 @@ async def register_ble_elm327_device(var, config):
     if CONF_FORMULA in config:
         formula_ = await cg.process_lambda(
             config[CONF_FORMULA],
-            [(cg.uint8, "a"), (cg.uint8, "b"), (cg.uint8, "c"), (cg.uint8, "d")],
+            [
+            (cg.uint8, "b"),
+            (cg.uint8, "c"),
+            (cg.uint8, "d"),
+            (cg.uint8, "e"),
+            (cg.uint8, "f"),
+            (cg.uint8, "g"),
+            (cg.uint8, "h"),
+            (cg.uint8, "i"),
+            (cg.uint8, "j"),
+            (cg.uint8, "k"),
+            (cg.uint8, "l")],
             return_type=cg.float_,
         )
         cg.add(var.set_formula(formula_))
