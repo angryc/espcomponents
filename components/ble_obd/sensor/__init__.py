4dae164012fe0ee252ebe67bddebade06271c28a
angryc <benjamin.proeger@googlemail.com>
2026-06-18 21:36:02 +0200

    Update __init__.py

--- components/ble_obd/sensor/__init__.py
+++ components/ble_obd/sensor/__init__.py
@@ -37,7 +37,6 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     var = await sensor.new_sensor(config)
-    await cg.register_component(var, config)
 
     paren = await cg.get_variable(config[CONF_BLE_OBD_ID])
     cg.add(paren.register_device(var))
